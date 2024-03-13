5af35f4658feee7b598227227414bca8d39ff85f
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2024-03-13 18:47:44 -0400

    Header section completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import css from './Header.module.css'
+import React from "react";
+import css from "./Header.module.css";
+import Logo from "../../assets/logo3.png";
+import { IoCartOutline} from "react-icons/io5"
 
 const Header = () => {
   return (
-    <div className={css.container}></div>
-  )
-}
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={Logo} alt="" />
+        <span>GlowGuru</span>
+      </div>
 
-export default Header
+      <div className={css.right}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+        <input type="text" className={css.search} placeholder="Search" />
+        <IoCartOutline className={css.cart}/>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
