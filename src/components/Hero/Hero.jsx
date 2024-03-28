b97f7c468becba3b90ae209324234d8c3fbcb00f
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2024-03-28 01:34:26 -0400

    animations added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,9 @@ import css from "./Hero.module.css";
 import HeroImg from "../../assets/hero.png";
 import { RiShoppingBagFill } from "react-icons/ri";
 import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
 const Hero = () => {
+  const transition = { duration: 3, type: "spring" };
   return (
     <div className={css.container}>
       {/**Left container */}
@@ -22,9 +24,28 @@ const Hero = () => {
 
       {/** Middle Hro container */}
       <div className={css.wrapper}>
-        <div className={css.bluecircle}></div>
-        <img src={HeroImg} alt="" width={600} />
-        <div className={css.cart2}>
+        <motion.div
+          initial={{ bottom: "2rem" }}
+          whileInView={{ bottom: "0rem" }}
+          className={css.bluecircle}
+          transition={transition}
+        ></motion.div>
+
+
+        <motion.img
+          transition={transition}
+          initial={{ bottom: "-2rem" }}
+          whileInView={{ bottom: "0rem" }}
+          src={HeroImg}
+          alt=""
+          width={600}
+        />
+
+
+        <motion.div 
+        transition={transition}
+        initial={{right: "4%"}}
+        whileInView={{right: "2%"}}  className={css.cart2}>
           <RiShoppingBagFill />
 
           <div className={css.signup}>
@@ -33,7 +54,7 @@ const Hero = () => {
               <BsArrowRight />
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       {/** Right container */}
